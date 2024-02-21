9fa13e20c179fca3d2c7215653cdc9a78c9e1711
VictorSamuel-dev <142364059+VictorSamuel-dev@users.noreply.github.com>
2024-02-20 23:34:32 -0500

    changes to src, webpack, package.json.

--- client/src-sw.js
+++ client/src-sw.js
@@ -1,5 +1,5 @@
 const { offlineFallback, warmStrategyCache } = require('workbox-recipes');
-const { CacheFirst } = require('workbox-strategies');
+const { CacheFirst, StaleWhileRevalidate } = require('workbox-strategies');
 const { registerRoute } = require('workbox-routing');
 const { CacheableResponsePlugin } = require('workbox-cacheable-response');
 const { ExpirationPlugin } = require('workbox-expiration');
@@ -27,9 +27,9 @@ warmStrategyCache({
 registerRoute(({ request }) => request.mode === 'navigate', pageCache);
 
 // TODO: Implement asset caching
-registerRoute(
-
-  ({ request }) => request.mode === 'navigate', pageCache);
+//registerRoute(
+//
+//  ({ request }) => request.mode === 'navigate', pageCache);
 
 // Set up asset cache
 registerRoute(
@@ -43,10 +43,10 @@ registerRoute(
       new CacheableResponsePlugin({
         statuses: [0, 200],
       }),
-      new ExpirationPlugin({
-        maxEntries: 60,
-        maxAgeSeconds: 30 * 24 * 60 * 60, //30 Days
-      })
+//      new ExpirationPlugin({
+//        maxEntries: 60,
+//        maxAgeSeconds: 30 * 24 * 60 * 60, //30 Days
+//      })
     ],
   })
 );
--- client/webpack.config.js
+++ client/webpack.config.js
@@ -4,7 +4,7 @@ const path = require('path');
 const { InjectManifest } = require('workbox-webpack-plugin');
 
 // TODO: Add and configure workbox plugins for a service worker and manifest file.
-const WorkboxPlugin = require("workbox-webpack-plugin");
+// const WorkboxPlugin = require("workbox-webpack-plugin");
 // TODO: Add CSS loaders and babel to webpack.
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
@@ -24,7 +24,7 @@ module.exports = () => {
         template: './index.html',
         title: 'JATE',
       }),
-      new MiniCssExtractPlugin(),
+//      new MiniCssExtractPlugin(),
       new InjectManifest({
         swSrc: './src-sw.js',
         swDest: 'src-sw.js',
